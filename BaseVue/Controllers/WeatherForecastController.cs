3c780e8b785f5650ce531dd888ee5e6a93aba394
phamdinhduong.it@gmail.com <phamdinhduong.it@gmail.com>
2022-08-13 01:11:50 +0700

    initial

---
+++ BaseVue/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaseVue.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("ThoiTiet")]
+        public IActionResult ThoiTiet()
+        {
+           var kq = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+            return new OkObjectResult(kq);
+        }
+    }
+}
---
+++ BaseVue/vueapp/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js Đại ca Dương1"/>
+    {{dulieu.temperatureC + " " + dulieu.summary}}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data()
+  {
+    return {
+        dulieu: ""
+    }
+  },
+  beforeCreate()
+  {
+    let self = this;
+    axios.get('/api/WeatherForecast/ThoiTiet')
+    .then(res => {
+       self.dulieu = {...res.data[0]}
+       console.log(self.dulieu);
+    }).catch(er =>
+    {
+      console.log(er);
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
